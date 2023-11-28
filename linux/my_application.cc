86707016bbbfcd8671804ad00761433369071efd
Robert Ancell <robert.ancell@canonical.com>
2023-11-28 14:32:02 +1300

    Adapt to GTK4

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -1,9 +1,9 @@
 #include "my_application.h"
 
 #include <flutter_linux/flutter_linux.h>
-#ifdef GDK_WINDOWING_X11
-#include <gdk/gdkx.h>
-#endif
+//#ifdef GDK_WINDOWING_X11
+//#include <gdk/gdkx.h>
+//#endif
 
 #include "flutter/generated_plugin_registrant.h"
 
@@ -28,34 +28,33 @@ static void my_application_activate(GApplication* application) {
   // If running on Wayland assume the header bar will work (may need changing
   // if future cases occur).
   gboolean use_header_bar = TRUE;
-#ifdef GDK_WINDOWING_X11
-  GdkScreen* screen = gtk_window_get_screen(window);
-  if (GDK_IS_X11_SCREEN(screen)) {
-    const gchar* wm_name = gdk_x11_screen_get_window_manager_name(screen);
-    if (g_strcmp0(wm_name, "GNOME Shell") != 0) {
-      use_header_bar = FALSE;
-    }
-  }
-#endif
+//#ifdef GDK_WINDOWING_X11
+//  GdkScreen* screen = gtk_window_get_screen(window);
+//  if (GDK_IS_X11_SCREEN(screen)) {
+//    const gchar* wm_name = gdk_x11_screen_get_window_manager_name(screen);
+//    if (g_strcmp0(wm_name, "GNOME Shell") != 0) {
+//      use_header_bar = FALSE;
+//    }
+//  }
+//#endif
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
-    gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "flutter_gtk4_test");
-    gtk_header_bar_set_show_close_button(header_bar, TRUE);
+    GtkWidget* header_label = gtk_label_new("flutter_gtk4_test");
+    gtk_header_bar_set_title_widget(header_bar, header_label);
+    gtk_header_bar_set_show_title_buttons(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
     gtk_window_set_title(window, "flutter_gtk4_test");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
-  gtk_widget_show(GTK_WIDGET(window));
+  gtk_window_present(window);
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
 
   FlView* view = fl_view_new(project);
-  gtk_widget_show(GTK_WIDGET(view));
-  gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
+  gtk_window_set_child(window, GTK_WIDGET(view));
 
   fl_register_plugins(FL_PLUGIN_REGISTRY(view));
 
